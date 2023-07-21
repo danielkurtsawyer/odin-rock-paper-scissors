7b266d77d7c4d2a409788b3f7210c82bfd1c7e16
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-21 13:09:45 +0800

    Add function playRound()
    This function simulates a round of rock, paper, scissors and returns the outcome of the round.
    This function takes two parameters:
        playerSelection - the player's choice of rock, paper, or scissors
        computerSelection - the computer's choice, generated with the getComputerSelection() function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,12 +1,40 @@
 function getComputerChoice() {
     let rand  = Math.floor(Math.random() * 3);
     if(rand === 0){
-        return 'Rock';
+        return 'ROCK';
     } else if(rand === 1){
-        return 'Paper';
+        return 'PAPER';
     }else {
-        return 'Scissors';
+        return 'SCISSORS';
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+
+    if(playerSelection === 'ROCK'){
+        if(computerSelection === 'PAPER'){
+            return 'You lose! Paper beats Rock';
+        } else if(computerSelection === 'SCISSORS'){
+            return 'You win! Rock beats Paper';
+        }
+    } else if(playerSelection === 'PAPER'){
+        if(computerSelection === 'ROCK'){
+            return 'You win! Rock beats Paper';
+        } else if(computerSelection === 'SCISSORS'){
+            return 'You lose! Scissors beats paper';
+        }
+    } else if(playerSelection === 'SCISSORS'){
+        if(computerSelection === 'ROCK'){
+            return 'You lose! Rock beats Scissors';
+        } else if(computerSelection === 'PAPER'){
+            return 'You win! Scissors beats Paper';
+        }
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 
