57b37b778f06747fce448263863cd2c2c6fab2d1
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-24 20:58:39 +0800

    Implement game logic with user button input
    This commit allows the user input to be sent into the event listener and the game logic is executed.
    After each round, the message div is updated with the status of the round, and the player or computer score is updated accordingly.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,13 @@
+let playerScore = 0; 
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+const message = document.querySelector('.message');
+const playerDiv = document.querySelector('.playerScore');
+const computerDiv = document.querySelector('.computerScore');
+
+buttons.forEach(btn => btn.addEventListener('click', playRound));
+
 function getComputerChoice() {
     let rand  = Math.floor(Math.random() * 3);
     if(rand === 0){
@@ -13,7 +23,43 @@ function playRound(e){
     computerSelection = getComputerChoice();
     playerSelection = e.target.classList.value;
 
-    
+    if(playerSelection === computerSelection){
+        message.textContent = 'Tie!';
+        return;
+    }
+
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'scissors'){
+            message.textContent = `${playerSelection} beats ${computerSelection}`;
+            playerScore++;
+        } else if(computerSelection === 'paper'){
+            message.textContent = `${computerSelection} beats ${playerSelection}`;
+            computerScore++;
+        }
+    } else if(playerSelection === 'paper'){
+        if(computerSelection === 'rock'){
+            message.textContent = `${playerSelection} beats ${computerSelection}`;
+            playerScore++;
+        } else if(computerSelection === 'scissors'){
+            message.textContent = `${computerSelection} beats ${playerSelection}`;
+            computerScore++;
+        }
+    } else if(playerSelection === 'scissors'){
+        if(computerSelection === 'paper'){
+            message.textContent = `${playerSelection} beats ${computerSelection}`;
+            playerScore++;
+        } else if(computerSelection === 'rock'){
+            message.textContent = `${computerSelection} beats ${playerSelection}`;
+            computerScore++;
+        }
+    }
+
+    console.log(playerScore.toString());
+    console.log(computerScore.toString());
+
+    playerDiv.textContent = playerScore.toString();
+    computerDiv.textContent = computerScore.toString();
+
     /*
     playerSelection = playerSelection.toUpperCase();
 
@@ -74,7 +120,3 @@ function game(){
         console.log('Computer wins the game!');
     }
 }
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(btn => btn.addEventListener('click', playRound));
